3b07e2757d1109a6acf51d900786e6657af60349
lihaorong <1911465106@qq.com>
2017-12-04 09:14:25 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/demo02/FouthFragment.java
@@ -0,0 +1,24 @@
+package com.example.administrator.demo02;
+
+/**
+ * Created by Administrator on 2017/12/3.
+ */
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FouthFragment extends Fragment {
+    public FouthFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fouth, container, false);
+    }
+}
